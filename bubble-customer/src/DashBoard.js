84e2b4db6f418db0a66c6aaebf109a04eb99e8b6
Gajendra-E <gajendragaja141@gmail.com>
2021-03-04 09:31:29 +0530

    second commit

---
+++ bubble-customer/src/DashBoard.js
@@ -0,0 +1,19 @@
+import {React,Component} from 'react';
+
+
+class Gajendra extends Component{
+    super(props);
+    constructor(){
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <div>
+                <Text>Welcome to Chennai</Text>
+            </div>
+        )
+    }
+
+}
